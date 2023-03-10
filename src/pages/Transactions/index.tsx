ecee2c33b3ae16c82d595d2adc1d66e47043fe2a
Dakson Cruz <daksonc@gmail.com>
2023-03-10 12:04:53 -0300

    feat(pages/Transactions) estrutura visual da tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from "../../components/Header";
 import { Summery } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summery />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvendo site</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>12/03/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Humburger</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 59,90
+                </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>12/03/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
