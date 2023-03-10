ccc52e038e72733900ed003a823b17d15cd4aa8e
Dakson Cruz <daksonc@gmail.com>
2023-03-10 11:01:56 -0300

    feat(components) estrutura visual do componente Summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/Header";
+import { Summery } from "../../components/Summary";
 
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summery />
     </div>
   )
 }
