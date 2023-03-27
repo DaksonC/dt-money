9523df3d4cd0b1e3e262ae41377509bf0dcd3ac2
Dakson Cruz <daksonc@gmail.com>
2023-03-27 13:08:39 -0300

    refactor: função de requisição das transações

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-export const instanceTransactions = axios.create({
+export const api = axios.create({
   baseURL: 'http://localhost:3333',
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from 'react';
-import { instanceTransactions } from '../api';
+import { api } from '../api';
 
 interface ITransactions {
   id: string;
@@ -25,14 +25,16 @@ export function TransactionsProvider({ children }: ITransactionsProviderProps) {
   const [transactions, setTransactions] = useState<ITransactions[]>([])
 
   async function fetchTransaction(query?: string) {
-    await instanceTransactions.get(`/transactions`)
-      .then(response => setTransactions(response.data))
-      .catch(error => console.log(error))
+    try {
+      const response = await api.get('/transactions', {
+        params: {
+          q: query
+        }
+      })
 
-    if (query) {
-      await instanceTransactions.get(`/transactions?q=${query}`)
-        .then(response => setTransactions(response.data))
-        .catch(error => console.log(error))
+      setTransactions(response.data)
+    } catch (error) {
+      console.log(error)
     }
   }
 
